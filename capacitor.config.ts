c9d17b7a61b4e36e67f58d6acbb02aaaf1e06d67
Ian Jerome Sebastian <ianjerome.sebastian.xentra@gmail.com>
2023-03-03 19:28:08 +0800

    Sample Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'firstapp-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
